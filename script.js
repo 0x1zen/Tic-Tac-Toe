3bc30ce67f628a594c93fcbefc4d45657171a410
0x1zen <rajdubalwork@gmail.com>
2023-05-28 16:40:44 +0530

    defined winning positions

---
+++ script.js
@@ -0,0 +1,16 @@
+const boxes=document.querySelectorAll(".box");
+const gameInfo=document.querySelector(".game-info");
+const  newGameBtn=document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+let winningPositions=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,3,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
